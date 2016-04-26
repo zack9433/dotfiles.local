d3f4edf33a1a24c33b480e30442476be03175243
Zack Yang <zack9433@gmail.com>
2016-04-26 14:58:22 +0800

    modify

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 "Installing dotfiles.local"
 
+echo "Installing oh-my-zsh"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 echo "Initializing submodule(s)"
 git submodule update --init --recursive
 
--- install/link.sh
+++ install/link.sh
@@ -1,11 +1,10 @@
+#!/bin/sh
+
 # Path to local dotfiles
 DOTFILES_LOCAL=$HOME/dotfiles.local
 
 # prompt pure theme -> https://github.com/sindresorhus/pure
-mkdir -p $HOME/.zfunctions
-fpath=("$HOME/.zfunctions" $fpath)
-[ ! -f "$HOME/.zfunctions/prompt_pure_setup" ] && ln -s "$DOTFILES_LOCAL/prompt/pure.zsh" "$HOME/.zfunctions/prompt_pure_setup"
-[ ! -f "$HOME/.zfunctions/async" ] && ln -s "$DOTFILES_LOCAL/prompt/async.zsh" "$HOME/.zfunctions/async"
+ln -sf "$DOTFILES_LOCAL/prompt/pure.zsh" "$HOME/.oh-my-zsh/themes/pure.zsh-theme"
 
 # create local config link
 [ ! -f "$HOME/.zshrc.local" ] && ln -s "$DOTFILES_LOCAL/.zshrc.local"
